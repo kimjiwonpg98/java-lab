4c2ee4d24ed865fcaf6dac9504ed566fbee9e87b
jiwonpg98 <jiwonpg98@gmail.com>
2023-04-04 00:15:23 +0900

    init: core

--- core/README.md
+++
@@ -1,6 +0,0 @@
-## 스프링 핵심 원리 - 기본편
-
-- setting
-  - preferences
-    - grade에 들어가서 build와 run을 intellij로 바꿔주자
-    - 빠른 이유때문에
--- core/src/main/java/hello/core/CoreApplication.java
+++ core/src/main/java/hello/core/CoreApplication.java
@@ -1,4 +1,4 @@
-package spring.base.core;
+package hello.core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
